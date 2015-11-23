3e122836e17e000b2d5620412eb6f6892428f895
ttsutsumi <plus223@gmail.com>
2015-11-23 17:07:25 -0500

    case sensivity for the search box has been fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   def index
     if params[:q]
       search_term = params[:q]
-      @products = Product.where("name LIKE ?", "%#{search_term}%")
+      @products = Product.where("LOWER(name) LIKE ? OR LOWER(description) LIKE ?", "%#{search_term.downcase}%", "%#{search_term.downcase}%")
     else
       @products = Product.all
     end
